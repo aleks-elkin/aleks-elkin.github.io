b3d20fabed75231a8ce09af605f1c3c66d6c3401
Aleksandr Elkin <55530374+aleks-elkin@users.noreply.github.com>
2024-12-06 18:42:50 +0100

    chore: switch to miniblog template

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,22 @@
+// @ts-check
 import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+
 import sitemap from "@astrojs/sitemap";
 
+import tailwind from "@astrojs/tailwind";
+import { SITE_URL } from "./src/consts";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: SITE_URL,
+  integrations: [mdx(), sitemap(), tailwind()],
+  markdown: {
+    shikiConfig: {
+      themes: {
+        light: "light-plus",
+        dark: "dark-plus",
+      },
+    },
+  },
+});
---
+++ src/consts.ts
@@ -0,0 +1,7 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_URL = "https://aleks-elkin.github.io";
+export const SITE_TITLE = "Aleks Elkin Blog";
+export const SITE_DESCRIPTION =
+  "Aleks Elkin personal blog about technology, programming, and other interesting topics.";
